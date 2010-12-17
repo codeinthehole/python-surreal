c57dc0eef30b4c2a90e56936bae3697ec4dbe675
David Winterbottom <david.winterbottom@gmail.com>
2010-12-17 09:28:44 +0000

    setup.py working

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='python-surreal',
+      version='0.1',
+      url='',
+      description="Surreal numbers implentation for Python",
+      author="David Winterbottom",
+      author_email="david.winterbottom@gmail.com",
+      package_dir={'': 'src'},
+      py_modules=['surreal'])

99dfd8265a7cd5a8777df369264c714afdf63236
David Winterbottom <david.winterbottom@gmail.com>
2010-12-22 17:05:17 +0000

    Updated tests to cover basic inequalities

--- src/surreal.py
+++ src/surreal.py
@@ -1,40 +1,88 @@
 import re
+import exceptions
+
+def dali(x):
+    """
+    For mapping the real numbers onto the surreals.
+
+    This takes a real number and maps it to the surreals.
+    """
+    if x == 0:
+        return Surreal(set(), set())
+    elif x > 0:
+        return dali(x-1)
+    elif x < 0:
+        return dali(x+1)
+
+def shorthand(raw):
+    """
+    Converts the written shorthand for Surreals into
+    a normal Surreal object
+    """
+    string = str(raw)
+
+    # Check if input is integer
+    if string.isdigit():
+        return dali(int(string))
+
+    # Check string for the {L|R} syntax
+    m = re.match(r'^{(.*)\|(.*)}$', string)
+    if m and len(m.groups()) == 2:
+        leftSurreals = set()
+        for symbol in m.group(1).split(","):
+            if symbol != "":
+                leftSurreals.add(shorthand(symbol))
+        rightSurreals = set()
+        for symbol in m.group(2).split(","):
+            if symbol != "":
+                rightSurreals.add(shorthand(symbol))
+        return Surreal(leftSurreals, rightSurreals)
+
+class BadlyFormed(exceptions.Exception):
+    pass
 
 class Surreal(object):
     """
     Surreal number
     """
-    def __init__(self, raw):
-        string = str(raw)
-        self.number = None
-        self.left = None
-        self.right = None
+    def __init__(self, leftSet, rightSet):
+        """
+        A Surreal number is initialised with a left and right
+        set, which both contain Surreal numbers.
+        """
+        self._is_well_formed(leftSet, rightSet)
+        self.leftSet = leftSet
+        self.rightSet = rightSet
 
-        # Check if is integer
-        if string.isdigit():
-            self.number = int(string)
-        # Check string for the {L|R} syntax
-        m = re.match(r'^{(.*)\|(.*)}$', string)
-        if m and len(m.groups()) == 2:
-            self.left = m.group(1)
-            self.right = m.group(2)
+    def _is_well_formed(self, leftSet, rightSet):
+        """
+        A Surreal number is well-formed is no member of the right set
+        is left-than or equal to a member of the left set.
+        """
+        for r in rightSet:
+            for l in leftSet:
+                if r <= l:
+                    raise BadlyFormed, "Left %s, right %s not well-formed" % (leftSet, rightSet)
+        
+    def __le__(self, other):
+        """
+        A Surreal number x is less than or equal to a surreal number y
+        if and only if y is less than or equal to no member of the left
+        set of x, and no member of the right set of y is less than or equal
+        to x.
+        """
+        for l in self.leftSet:               
+            if other <= l: 
+                return False
+        for r in other.rightSet:
+            if r <= self:
+                return False
+        return True
+
 
     def __eq__(self, other):
-        return int(self) == int(other)
-
-    def __int__(self):
-        if self.number != None:
-            return int(self.number)
-        if self.left == '' and self.right == '':
-            return 0
-        if self.left == '1':
-            return 1
+        return self <= other and other <= self
 
     def __repr__(self):
-        if self.left != None and self.right != None:
-            return '{%s|%s}' % (self.left, self.right)
-
-class Zero(Surreal):
-    def __init__(self):
-        super(Zero, self).__init__('', '')
+        return 'S{%s|%s}' % (self.leftSet, self.rightSet)
 
--- tests.py
+++ tests.py
@@ -1,14 +1,74 @@
 import unittest
 
-from surreal import Surreal as S
+from surreal import dali, shorthand, Surreal as S, BadlyFormed
+
+class DaliTests(unittest.TestCase):
+    def testZero(self):
+        self.assertEquals(S(set(), set()), dali(0))
+    def testOne(self):
+        self.assertRuleForPositiveIntegers(1)
+    def testSetOfPositiveIntegers(self):
+        for x in range(1, 100):
+            self.assertRuleForPositiveIntegers(x)
+    def testMinusOne(self):
+        self.assertRuleForNegativeIntegers(1)
+    def testSetOfNegativeIntegers(self):
+        for x in range(-1, -100):
+            self.assertRuleForNegativeIntegers(x)
+
+    def assertRuleForPositiveIntegers(self, x):
+        self.assertEquals(dali(x-1), dali(x))
+    def assertRuleForNegativeIntegers(self, x):
+        self.assertEquals(dali(x+1), dali(x))
 
 class SurrealTests(unittest.TestCase):
-    equivalent_symbols = (('{|}', 0),
-                          ('{1|}', 1))
+    well_formed = ((set(), set()),)
 
-    def test_equivalent_symbols(self):
+    def test_well_formed_numbers(self):
+        for left, right in self.well_formed:
+            S(left, right)
+
+    def test_inequality_of_zero(self):
+        assert S(set(), set()) <= S(set(), set())
+
+class ShorthandTests(unittest.TestCase):
+    equivalent_symbols = (('{|}',  0),
+                          ('{0|}', 1),
+                          ('{1|}', 2),
+                          ('{|0}', -1),
+                          )
+
+    well_formed = ('{|}', '0', '{0|}', '{|0}')
+    not_well_formed = ('{0|0}',)
+    le_inequalities = (('{|}', '{0|}'),
+                       ('{0|}', '{0|}'))
+
+    def test_well_formed_shorthands(self):
+        for sh in self.well_formed:
+            try:
+                assert type(shorthand(sh)) == S
+            except BadlyFormed:
+                self.fail("%s should be well-formed" % sh)
+
+    def test_not_well_formed_shorthands(self):
+        for sh in self.not_well_formed:
+            try:
+                shorthand(sh)
+                self.fail("%s should raise a BadlyFormed exception" % sh)
+            except BadlyFormed:
+                pass
+
+    def test_le_inequalities(self):
+        for left, right in self.le_inequalities:
+            self.assertTrue(shorthand(left) <= shorthand(right))
+
+    def _test_equivalent_symbols(self):
         for left, right in self.equivalent_symbols:
-            self.assertTrue(S(left) == S(right), "%s should equal %s" % (left, right))
+            leftSurreal = shorthand(left)
+            rightSurreal = shorthand(right)
+            self.assertTrue(leftSurreal == rightSurreal, 
+                    "%s should equal %s (Surreal: %s == %s)" % 
+                    (left, right, leftSurreal, rightSurreal))
 
 if __name__ == '__main__':
     unittest.main()
